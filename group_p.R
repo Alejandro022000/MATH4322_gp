0feb6a8be5e100f9eb7b3b11ac693d38ca10d670
Alejandro Perez Gonzalez <ale07pg@gmail.com>
2023-03-22 23:35:50 -0500

    .

--- group_p.R
+++ group_p.R
@@ -1,2 +1,7 @@
-data <- read.table("C:/Users/ale07/OneDrive/School/Spring 2023/Data Science and Machine Learning/Group project/winequality-white.csv", header=TRUE,  sep = ";")
-s
+data <- read.table("C:/Users/ale07/OneDrive/School/Spring 2023/Data Science and Machine Learning/Group project/winequality-white.csv", header=TRUE,  sep = ";") # nolint
+#create linear regression model with all variables and quality as the response
+model <- lm(quality ~ ., data = data)
+#summary(model)
+summary(model)
+#plot(model)
+plot(model)
