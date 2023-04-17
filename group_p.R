8fdf36e0760a03cc5bf3f1e5a34928d105357ba3
Alejandro022000 <89061681+Alejandro022000@users.noreply.github.com>
2023-04-17 14:13:54 -0500

    Delete group_p.R

--- group_p.R
+++
@@ -1,7 +0,0 @@
-data <- read.table("C:/Users/ale07/OneDrive/School/Spring 2023/Data Science and Machine Learning/Group project/winequality-white.csv", header=TRUE,  sep = ";") # nolint
-#create linear regression model with all variables and quality as the response
-model <- lm(quality ~ ., data = data)
-#summary(model)
-summary(model)
-#plot(model)
-plot(model)
